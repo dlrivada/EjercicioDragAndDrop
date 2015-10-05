471af63d4ee1b490aed81d414c14bfb1e6071ede
David Lozano Rivada <dlrivada@hotmail.com>
2015-10-05 12:54:34 +0200

    05/10/2015

---
+++ EjercicioDragAndDrop/DragAndDrop.js
@@ -0,0 +1,34 @@
+﻿
+if (!sessionStorage.getItem("nombre")) {
+    location.replace("login.html");
+}
+
+
+var objetoTrasladado = undefined;
+
+function drag(evento) {
+    objetoTrasladado = evento.target.id;
+    //evento.dataTransfer.setData("objeto", evento.target.id);
+}
+
+function allowDrop(evento) {
+    evento.preventDefault();
+}
+
+function drop(evento) {
+    evento.preventDefault();
+    //var data = evento.dataTransfer.getData("objeto");
+    var data = objetoTrasladado;
+    document.getElementById("divDestino").appendChild(document.getElementById(data));
+    objetoTrasladado = undefined;
+}
+
+var imgs = document.querySelectorAll("img");
+var destino = document.querySelector("#divDestino");
+
+for (var i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener("dragstart", drag);
+}
+
+destino.addEventListener("dragover", allowDrop);
+destino.addEventListener("drop", drop);
---
+++ EjercicioDragAndDrop/login.js
@@ -0,0 +1,15 @@
+﻿if (sessionStorage.getItem("nombre")) {
+    location.replace("index.html");
+}
+
+function login() {
+    if (document.getElementById("txtUsuario").value === "") {
+        alert("Nombre vacío");
+        return;
+    }
+    sessionStorage.setItem("nombre", document.getElementById("txtUsuario").value);
+    location.replace("index.html");
+    
+}
+
+document.getElementById("btnOk").addEventListener("click", login);
